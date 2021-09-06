edcf8222ca067f2742d6d4248e96f7ac4a8fc4cd
Boin-Kau <charles2323@softsquared.com>
2021-09-07 00:37:51 +0900

    modify bottom info

--- week1/week1_clone1/index.js
+++ week1/week1_clone1/index.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// sticky header Event
 const header = document.querySelector('header');
 const headerHeight = header.getBoundingClientRect().height;
 
@@ -10,3 +11,7 @@ window.addEventListener("scroll", () => {
     header.setAttribute("style", "background: transparent;");
   }
 });
+
+// 질문 1. js 파일은 어떻게 관리해야 되는지 
+// 질문 2. 하나의 js 파일에는 어떤 코드를 모으는게 적절한지? 
+// 질문 2-1. 그 기준이 궁금. ex.화면별, 섹션별
