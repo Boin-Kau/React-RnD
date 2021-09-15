23bd5053a88e7ae95f8a4a3195677d8fbe401ad9
Boin-Kau <charles2323@softsquared.com>
2021-09-16 01:03:17 +0900

    add header

---
+++ week2/week2_clone/index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+// sticky header Event
+const header = document.querySelector('header');
+const headerHeight = header.getBoundingClientRect().height;
+
+window.addEventListener("scroll", () => {
+  if(window.scrollY > headerHeight) {
+    header.setAttribute("style", "background: black;");
+  } else {
+    header.setAttribute("style", "background: transparent;");
+  }
+});
